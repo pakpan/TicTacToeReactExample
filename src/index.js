f569a7c1a5154644e7639cdba876ee92ac80c664
pakpan <pakpan3@gmail.com>
2021-12-23 18:31:48 -0500

    Added status change for ties

--- src/index.js
+++ src/index.js
@@ -96,6 +96,8 @@ class Game extends React.Component {
     let status;
     if(winner){
       status = 'Winner: ' + winner;
+    } else if(this.state.stepNumber === 9 && !winner){
+      status = "It's a tie!"
     } else{
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
